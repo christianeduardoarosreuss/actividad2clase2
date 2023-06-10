1e90dd8bc43eb2050465786ea1bfad14d0574394
Christian Aros <christianarosreuss@gmail.com>
2023-06-09 20:09:38 -0400

    test1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -38,6 +38,46 @@ pipeline {
                 }
             }
         }
+def COLOR_MAP = [
+    'SUCCESS': 'good', 
+    'FAILURE':'danger', 
+    ]
+
+pipeline {
+    agent any
+
+    tools {
+        maven 'jenkinsmaven'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/christianeduardoarosreuss/actividad2clase2.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                script {
+                    sh 'mvn clean package'
+                }
+            }
+        }
+
+        stage('Archive Artifacts') {
+            steps {
+                archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                    sh 'mvn test'
+                }
+            }
+        }
 
         post{
         always{
@@ -47,5 +87,7 @@ pipeline {
             message: "*${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More Info at: ${env.BUILD_URL}"
         }
     }
+    }
+}
     }
 }
